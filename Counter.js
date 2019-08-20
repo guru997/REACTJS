370f8ea5834fea63a6913b84204a3b9b4c1b7394
guru prasath <guruprasath997@gmail.com>
2019-08-20 22:14:14 +0530

    Add files via upload

--- Counter.js
+++ Counter.js
@@ -13,23 +13,14 @@ export class Counter extends Component
     
 
 increment()
-{ 
-
-    this.setState((prevState) => (
-        {
-            count: prevState.count +1
-        }
-    ))
-    console.log(this.state.count)
-}
-
-incrementFive()
 {
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
+    this.setState(
+
+        {
+            count: this.state.count + 1
+        }, () => { console.log('Callback value', this.state.count) }
+    )
+    console.log(this.state.count)
 }
 
 
@@ -38,7 +29,7 @@ render()
     return(
         <div>
            <div> Count - {this.state.count} </div>
-           <button onClick = { () => this.incrementFive() }>Increment</button>
+           <button onClick = { () => this.increment() }>Increment</button>
         </div>
     )
 }
