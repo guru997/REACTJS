8249d3c948c3a55973c29d0db495c49e7299e649
guru prasath <guruprasath997@gmail.com>
2019-08-25 12:25:23 +0530

    Add files via upload

---
+++ Button click/ClassClick.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+
+clickHandler()
+{
+    console.log('Clicked the Button')
+}
+
+
+    render() {
+        return (
+            <div>
+                <button onClick = {this.clickHandler }>Click Me</button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick
