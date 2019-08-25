5430dcf463d09278d5fd487b3e0f4c5ebcf3d893
guru prasath <guruprasath997@gmail.com>
2019-08-25 14:12:59 +0530

    Add files via upload

---
+++ Parent to child/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChildComponent() {
+    return (
+        <div>
+            <button onClick = {() => this.props.greetHandler('child') }>Greet Parent</button>
+        </div>
+    )
+}
+
+export default ChildComponent
