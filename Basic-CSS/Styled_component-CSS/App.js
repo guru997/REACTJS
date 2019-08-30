41cca7a7395217b024b8f487242ac99fdf6d21b5
guru prasath <guruprasath997@gmail.com>
2019-08-30 22:50:10 +0530

    Update App.js

--- Basic-CSS/Styled_component-CSS/App.js
+++ Basic-CSS/Styled_component-CSS/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/welcome'
-import Message from './components/Message'
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
+
 
 import './appStyle.css'
 import styles from './appStyle.modules.css'
@@ -22,24 +11,7 @@ function App() {
     <div className="App">
       <h1 className = 'error'>Error</h1>
       <h1 className= { styles.success }>Success</h1>
-      {/* <Inline /> */}
-      {/* <Stylesheet primary = { false } /> */}
-      {/* <NameList /> */}
-      {/* <UserGreeting />  */}
-      {/* <ParentComponent /> */}
-      {/* <Message /> */}
-      {/* <Counter/> */}
-      {/* <FunctionClick />
-      <ClassClick /> */}
-      {/*  <EventBind />  */}
-      {/* <Greet name = "Bruce" heroname ="Batman">
-      <p>This is chidren props</p></Greet>
-      <Greet name = "Clark" heroname = "supername"/>
-      <button>Action</button>
-      <Greet name = "Diana" heroname = "Wonder woman"/>
-      <Welcome name = "Bruce" heroname ="Batman"/>
-      <Welcome name = "Clark" heroname ="Superman"/>
-      <Welcome name = "Diana" heroname ="Wonder Woman"/> */}
+     
     </div>
   );
 }
